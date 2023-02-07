0e5dcb226a12d60584e635c260d32d572c8c0a79
Luis G. Villegas <luisguveal@gmail.com>
2023-02-07 09:47:08 -0400

    Set margin-bottom

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-fluid px-0 mb-5">
+  <div class="container-fluid px-0">
     <Banner />
     <Articles />
     <Newsletter />
@@ -13,3 +13,9 @@ export default Vue.extend({
   name: 'IndexPage'
 })
 </script>
+
+<style scoped>
+  .container-fluid {
+    margin-bottom: 150px;
+  }
+</style>
